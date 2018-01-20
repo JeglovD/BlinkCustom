a1e0fbf49cddc847129ad9b3cd3c298be3c24819
jeglov <jeglovd@gmail.com>
2018-01-20 14:10:13 +0300

    ...

---
+++ BlinkCustom/BlinkCustom.ino
@@ -0,0 +1,39 @@
+#include <StandardCplusplus.h>
+#include <serstream.h>
+
+const byte interruptionPin(2);
+
+volatile unsigned long fallingMillis;
+volatile bool power(false);
+
+namespace std
+{
+	ohserialstream cout(Serial);
+}
+
+void Interruption()
+{
+	if (millis() - fallingMillis > 1000)
+	{
+		std::cout << millis() - fallingMillis << std::endl;
+		fallingMillis = millis();
+
+		power = !power;
+		digitalWrite(LED_BUILTIN, power);
+	}
+}
+
+void setup() 
+{
+	Serial.begin(115200);
+
+	fallingMillis = millis();
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(interruptionPin, INPUT);
+	digitalWrite(interruptionPin, HIGH);
+	attachInterrupt(0, Interruption, FALLING);
+}
+
+void loop() 
+{
+}
---
+++ BlinkCustom/Release/BlinkCustom.cpp.d
@@ -0,0 +1,47 @@
+C:\Users\DJ\AppData\Local\Temp\VMBuilds\BlinkCustom\pro_16MHzatmega328\Release\BlinkCustom.cpp.o: \
+ C:\Users\DJ\AppData\Local\Temp\VMBuilds\BlinkCustom\pro_16MHzatmega328\Release\BlinkCustom.cpp \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ d:\_Arduino\libraries\StandardCplusplus-master/StandardCplusplus.h \
+ d:\_Arduino\libraries\StandardCplusplus-master/serstream.h \
+ d:\_Arduino\libraries\StandardCplusplus-master/basic_definitions \
+ d:\_Arduino\libraries\StandardCplusplus-master/system_configuration.h \
+ d:\_Arduino\libraries\StandardCplusplus-master/iosfwd \
+ d:\_Arduino\libraries\StandardCplusplus-master/char_traits \
+ d:\_Arduino\libraries\StandardCplusplus-master/exception \
+ d:\_Arduino\libraries\StandardCplusplus-master/memory \
+ d:\_Arduino\libraries\StandardCplusplus-master/new \
+ d:\_Arduino\libraries\StandardCplusplus-master/cstddef \
+ d:\_Arduino\libraries\StandardCplusplus-master/cstdlib \
+ d:\_Arduino\libraries\StandardCplusplus-master/iterator_base \
+ d:\_Arduino\libraries\StandardCplusplus-master/utility.h \
+ d:\_Arduino\libraries\StandardCplusplus-master/cstdio \
+ d:\_Arduino\libraries\StandardCplusplus-master/ios \
+ d:\_Arduino\libraries\StandardCplusplus-master/locale \
+ d:\_Arduino\libraries\StandardCplusplus-master/string \
+ d:\_Arduino\libraries\StandardCplusplus-master/func_exception \
+ d:\_Arduino\libraries\StandardCplusplus-master/vector \
+ d:\_Arduino\libraries\StandardCplusplus-master/iterator \
+ d:\_Arduino\libraries\StandardCplusplus-master/algorithm \
+ d:\_Arduino\libraries\StandardCplusplus-master/functional \
+ d:\_Arduino\libraries\StandardCplusplus-master/type_traits \
+ d:\_Arduino\libraries\StandardCplusplus-master/initializer_list \
+ d:\_Arduino\libraries\StandardCplusplus-master/istream \
+ d:\_Arduino\libraries\StandardCplusplus-master/cctype \
+ d:\_Arduino\libraries\StandardCplusplus-master/streambuf \
+ d:\_Arduino\libraries\StandardCplusplus-master/istream_helpers \
+ d:\_Arduino\libraries\StandardCplusplus-master/ostream \
+ d:\_Arduino\libraries\StandardCplusplus-master/ostream_helpers \
+ d:\_Arduino\libraries\StandardCplusplus-master/iostream \
+ d:\_Arduino\libraries\StandardCplusplus-master/string_iostream \
+ c:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h
